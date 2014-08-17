31eda7f48b9054a07263346a3eab2b2ae49a5a07
haskell <haskell@henning-thielemann.de>
2014-08-17 22:48:57 +0000

    triangle2 -> poly2: it is important to intersect only lines from different polygons
    demonstrate that with overlapping boxes

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,13 @@ import qualified Codec.Picture as Pic
 
 import qualified Polygon
 import qualified Line
-import SegmentIntersection (naiveIntersections)
 import Polygon (Polygon(PolygonCW))
 import Line (Line(Segment))
 import Point2 (Point2(Point2), distance)
 
+import Control.Monad (liftM2)
 import Data.Maybe.HT (toMaybe)
-import Data.Maybe (mapMaybe)
+import Data.Maybe (catMaybes, mapMaybe)
 
 
 project ::
@@ -21,17 +21,26 @@ project x (a, b) =
    let (r, _, _, y) = Line.distanceAux a b x
    in  toMaybe (0<=r && r<=1) y
 
-triangle2 :: Pic.Image Pic.PixelRGB8
-triangle2 =
-   let points0 = [Point2 (10, 10), Point2 (245, 100), Point2 (100, 245)]
-       points1 = map (\(Point2 (x,y)) -> Point2 (255-x,y)) points0
-       poly0 = PolygonCW points0
+triA, triB :: [Point2 Double]
+triA = [Point2 (10, 10), Point2 (245, 100), Point2 (100, 245)]
+triB = map (\(Point2 (x,y)) -> Point2 (255-x,y)) triA
+
+boxA, boxB :: [Point2 Double]
+boxA = [Point2 (10, 10), Point2 (200, 10), Point2 (200, 200), Point2 (10, 200)]
+boxB = [Point2 (55, 55), Point2 (245, 55), Point2 (245, 245), Point2 (55, 245)]
+
+poly2 ::
+   (Floating a, RealFrac a) =>
+   [Point2 a] -> [Point2 a] -> Pic.Image Pic.PixelRGB8
+poly2 points0 points1 =
+   let poly0 = PolygonCW points0
        poly1 = PolygonCW points1
        segments = map (uncurry Segment) . Polygon.edges
-       intPoints = naiveIntersections $ segments points0 ++ segments points1
+       intPoints =
+          catMaybes $
+          liftM2 Line.intersect (segments points0) (segments points1)
        render xi yi =
-          let x, y :: Double
-              x = fromIntegral xi
+          let x = fromIntegral xi
               y = fromIntegral yi
               p = Point2 (x,y)
               minDist0 =
@@ -80,6 +89,7 @@ gradient =
 
 main :: IO ()
 main = do
-   PNG.writePng "/tmp/triangle2.png" triangle2
+   PNG.writePng "/tmp/box2.png" $ poly2 boxA boxB
+   PNG.writePng "/tmp/triangle2.png" $ poly2 triA triB
    PNG.writePng "/tmp/triangle.png" triangle
    PNG.writePng "/tmp/test.png" gradient
